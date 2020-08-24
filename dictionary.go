6a4e21c58148c7c3c3f635cce8d4fe7b9ff425d6
Michael Draper <michael@owlbrite.com>
2020-08-24 08:45:39 -0700

    added documentation and comments

--- dictionary.go
+++ dictionary.go
@@ -6,12 +6,22 @@ type ExcludeWord func(s string)bool
 type TransformWord func(s string) string
 
 type DictionaryConfig struct {
+	// Limits the lower bound to the length of words in the list
 	MinLength int
+	// Limits the upper bound to the length of words in the list
 	MaxLength int
+	// If this pointer is non-nil, this list is used instead of
+	// the default OS dictionary.
 	CustomWordList *[]string
+	// Changes all letters in each word to lower case
 	Downcase bool
+	// Changes all letters in each word to upper case
 	Upcase bool
+	// If present, this function is emitted on all words in the dictionary,
+	// if the provided function returns true, the word is not included in the
+	// word list.
 	ExcludeWord ExcludeWord
+	// Emittted on all words in the dictionary
 	TransformWord TransformWord
 }
 var  DefaultDictionaryConfig = DictionaryConfig {
@@ -29,7 +39,15 @@ type babbleDictionary struct {
 	sourceList []string
 	config *DictionaryConfig
 }
-
+// NewDictionaryWithConfig creates a new dictionary with the supplied
+// configuration struct.
+//    c = babble.DictionaryConfig {
+//				MinLength: 3,
+//				MaxLength: 5,
+//				Downcase: true,
+//
+//        }
+//    d = NewDictionaryWithConfig(c)
 func NewDictionaryWithConfig(c DictionaryConfig) Dictionary {
 	if c.Upcase && c.Downcase {
 		panic("cannot upcase and downcase the dictionary at the same time, invalid dictionary config")
@@ -54,15 +72,23 @@ func NewDictionaryWithConfig(c DictionaryConfig) Dictionary {
 	}
 	return d
 }
+
+// GetRandomWord returns a random word from the dictionary
 func (d *babbleDictionary) GetRandomWord() string {
 	if d.config.MinLength > d.config.MaxLength {
 		panic("minimum length cannot exceed maximum length")
 	}
   return getRandomWordFromList(d.config.MinLength, d.config.MaxLength,d.sourceList)
 }
+
+// GetListLength returns the length of the currently loaded word list.
 func (d *babbleDictionary) GetListLength() int {
 	return len(d.sourceList)
 }
+
+// applyTransform applies the configured transformation function
+// on each string, and if Upcase or Downcase is true, that
+// transformation is applied
 func (d *babbleDictionary) applyTransform(s string) (ts string) {
 	txf := d.config.TransformWord
 	if txf != nil {
@@ -78,9 +104,15 @@ func (d *babbleDictionary) applyTransform(s string) (ts string) {
 	}
 	return ts
 }
+
+// GetWordList returns the entire list of possible words
 func (d *babbleDictionary) GetWordList() []string {
 	return d.sourceList
 }
+
+// loadWithExclude loads the list of possible words, emitting
+// the ExcludeWord function if present to filter candidate
+// words.
 func (d *babbleDictionary) loadWithExclude() {
 	exc := d.config.ExcludeWord
 	newSource := []string{}
@@ -91,6 +123,8 @@ func (d *babbleDictionary) loadWithExclude() {
 	}
 	d.sourceList = newSource
 }
+
+// load loads the list of possible words
 func (d *babbleDictionary) load()  {
 	 list := readAvailableDictionary()
 	 d.sourceList = GenerateEligibleWordList(list, d.config.MinLength, d.config.MaxLength)
